9a20dfd2d1b00bf13395d7d04664003688a58cc0
Steve Casey <stevencasey21@gmail.com>
2019-10-06 10:02:22 -0400

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ readme = open("README.rst").read()
 
 setup(
     name="stalker_comment_editor",
-    version="0.3.0",
+    version="0.3.1",
     description="Cli tool for bulk editing of stalker ogg vorbis comments.",
     long_description=readme,
     author="Steve Casey",
